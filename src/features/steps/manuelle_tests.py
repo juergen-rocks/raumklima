496e49b5db2d37854d55c114afff8230fd2fadb7
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2022-03-28 12:20:57 +0200

    Reformat manuelle_tests.py

--- src/features/steps/manuelle_tests.py
+++ src/features/steps/manuelle_tests.py
@@ -5,12 +5,12 @@ from behave import *
 
 def manual_step(question: str, answers: list = None, pass_answers: list = None, answers_case_insensitive: bool = True):
     if answers is None:
-        answers = ['y', 'j', 'n']
+        answers = ["y", "j", "n"]
     if pass_answers is None:
-        pass_answers = ['y', 'j']
+        pass_answers = ["y", "j"]
     answer = None
     while answer not in answers:
-        print('    * ' + question, flush=True, file=stdout)
+        print("    * " + question, flush=True, file=stdout)
         answer = input()
         if answers_case_insensitive:
             answer = answer.lower()
@@ -19,17 +19,17 @@ def manual_step(question: str, answers: list = None, pass_answers: list = None, 
 
 @step("ein Raspberry Pi mit einer Installation gemäß Anleitung")
 def step_impl(context):
-    manual_step('Ist das Raspberry Pi installiert und bereit? (j/n)')
+    manual_step("Ist das Raspberry Pi installiert und bereit? (j/n)")
 
 
 @step("eine verbundene Raumklimastation")
 def step_impl(context):
-    manual_step('Ist die Raumklimasstation verbunden? (j/n)')
+    manual_step("Ist die Raumklimasstation verbunden? (j/n)")
 
 
 @step("mindestens zwei verbundene Sensoren")
 def step_impl(context):
-    manual_step('Sind mindestens die Sensoren 1 und 2 verbunden? (j/n)')
+    manual_step("Sind mindestens die Sensoren 1 und 2 verbunden? (j/n)")
 
 
 @step("das Python-Skript '{script}' ausgeführt wird")
@@ -39,9 +39,9 @@ def step_impl(context, script):
 
 @step("wird für den ersten Kanal richtige Werte ausgegeben")
 def step_impl(context):
-    manual_step('Hat das Skript für Kanal 1 korrekte Werte angezeigt? (j/n)')
+    manual_step("Hat das Skript für Kanal 1 korrekte Werte angezeigt? (j/n)")
 
 
 @step("wird für den zweiten Kanal richtige Werte ausgegeben")
 def step_impl(context):
-    manual_step('Hat das Skript für Kanal 2 korrekte Werte angezeigt? (j/n)')
+    manual_step("Hat das Skript für Kanal 2 korrekte Werte angezeigt? (j/n)")
